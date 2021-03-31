deca844628d491e2f43c9a07406e744794c7c770
Dumitru Murar <dumitru.murar@endava.com>
2021-03-31 14:13:34 +0300

    First Commit

--- src/GildedRose.Console/Program.cs
+++ src/GildedRose.Console/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 
+
 namespace GildedRose.Console
 {
     class Program
@@ -10,23 +11,18 @@ namespace GildedRose.Console
             System.Console.WriteLine("OMGHAI!");
 
             var app = new Program()
-                          {
-                              Items = new List<Item>
+            {
+                Items = new List<Item>
                                           {
                                               new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
                                               new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
                                               new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
                                               new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
-                                              new Item
-                                                  {
-                                                      Name = "Backstage passes to a TAFKAL80ETC concert",
-                                                      SellIn = 15,
-                                                      Quality = 20
-                                                  },
+                                              new Item {Name = "Backstage passes to a TAFKAL80ETC concert",SellIn = 15, Quality = 20 },
                                               new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
                                           }
 
-                          };
+            };
 
             app.UpdateQuality();
 
@@ -44,7 +40,7 @@ namespace GildedRose.Console
                     {
                         if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
                         {
-                            Items[i].Quality = Items[i].Quality - 1;
+                            Items[i].Quality--;
                         }
                     }
                 }
@@ -52,7 +48,7 @@ namespace GildedRose.Console
                 {
                     if (Items[i].Quality < 50)
                     {
-                        Items[i].Quality = Items[i].Quality + 1;
+                        Items[i].Quality++;
 
                         if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
                         {
@@ -60,7 +56,7 @@ namespace GildedRose.Console
                             {
                                 if (Items[i].Quality < 50)
                                 {
-                                    Items[i].Quality = Items[i].Quality + 1;
+                                    Items[i].Quality++;
                                 }
                             }
 
@@ -68,7 +64,7 @@ namespace GildedRose.Console
                             {
                                 if (Items[i].Quality < 50)
                                 {
-                                    Items[i].Quality = Items[i].Quality + 1;
+                                    Items[i].Quality++;
                                 }
                             }
                         }
@@ -77,7 +73,7 @@ namespace GildedRose.Console
 
                 if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
                 {
-                    Items[i].SellIn = Items[i].SellIn - 1;
+                    Items[i].SellIn--;
                 }
 
                 if (Items[i].SellIn < 0)
@@ -90,20 +86,20 @@ namespace GildedRose.Console
                             {
                                 if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
                                 {
-                                    Items[i].Quality = Items[i].Quality - 1;
+                                    Items[i].Quality--;
                                 }
                             }
                         }
                         else
                         {
-                            Items[i].Quality = Items[i].Quality - Items[i].Quality;
+                            Items[i].Quality -= Items[i].Quality;
                         }
                     }
                     else
                     {
                         if (Items[i].Quality < 50)
                         {
-                            Items[i].Quality = Items[i].Quality + 1;
+                            Items[i].Quality++;
                         }
                     }
                 }
@@ -112,13 +108,7 @@ namespace GildedRose.Console
 
     }
 
-    public class Item
-    {
-        public string Name { get; set; }
 
-        public int SellIn { get; set; }
 
-        public int Quality { get; set; }
-    }
 
 }
