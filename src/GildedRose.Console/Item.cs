cdbc0de56e4919eb78a6d281841e266930e806ff
Dumitru Murar <dumitru.murar@endava.com>
2021-04-09 10:37:48 +0300

    Best try

--- src/GildedRose.Console/Item.cs
+++ src/GildedRose.Console/Item.cs
@@ -14,6 +14,86 @@ namespace GildedRose.Console
         public int SellIn { get; set; }
 
         public int Quality { get; set; }
+
+        public IList<Item> Items;
+        public void UpdateQuality()
+        {
+            for (var i = 0; i < Items.Count; i++)
+            {
+                if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                {
+                    if (Items[i].Quality > 0 && Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                    {
+                        Items[i].Quality--;
+                    }
+                }
+                else
+                {
+                    if (Items[i].Quality < 50)
+                    {
+                        Items[i].Quality++;
+
+                        if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
+                        {
+                            if (Items[i].SellIn < 11 && Items[i].Quality < 50)
+                            {
+                                Items[i].Quality++;
+                            }
+
+                            if (Items[i].SellIn < 6 && Items[i].Quality < 50)
+                            {
+                                Items[i].Quality++;
+                            }
+                        }
+                    }
+                }
+
+                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                {
+                    Items[i].SellIn--;
+                }
+
+                if (Items[i].SellIn < 0)
+                {
+                    if (Items[i].Name != "Aged Brie")
+                    {
+                        if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                        {
+                            if (Items[i].Quality > 0 && Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                            {
+                                Items[i].Quality--;
+                            }
+                        }
+                        else
+                        {
+                            Items[i].Quality -= Items[i].Quality;
+                        }
+                    }
+                    else
+                    {
+                        if (Items[i].Quality < 50)
+                        {
+                            Items[i].Quality++;
+                        }
+                    }
+                }
+            }
+        }
+        public IList<Item> GetItem()
+        {
+
+
+            Items = new List<Item>
+                                          {
+                                              new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
+                                              new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
+                                              new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
+                                              new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
+                                              new Item {Name = "Backstage passes to a TAFKAL80ETC concert",SellIn = 15, Quality = 20 },
+                                              new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
+                                          };
+            return Items;
+        }
     }
 }
 
